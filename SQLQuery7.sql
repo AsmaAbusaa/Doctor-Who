c2b05e61355cbe886c237e491f46347b8afa19a0
Asma Abu Saa <asmaabusaa4@gmail.com>
2023-02-02 17:01:11 +0200

    fnEnemy

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,9 +1,16 @@
-CREATE FUNCTION fnEnemies(
+CREATE FUNCTION fnEnemies1(
 @episodeId INT
 )
-RETURNS TABLE AS
-RETURN
-SELECT CONCAT('Enemy ',ROW_NUMBER() OVER(ORDER BY E.EnemyId ASC),EnemyName)AS Enemies_Name
+RETURNS  varchar(255) AS
+BEGIN
+DECLARE @count INT
+DECLARE @result varchar(255)
+SET @count=1
+SET @result =''
+
+SELECT @result = @result+CONCAT('Enemy ',@count)+' '+ E.EnemyName+', ',@count=@count+1
 FROM tblEnemy as E INNER JOIN tblEpisodeEnemy as EE
 ON E.EnemyId=EE.EnemyId
 WHERE EE.EpisodeId= @episodeId
+return @result
+END;
