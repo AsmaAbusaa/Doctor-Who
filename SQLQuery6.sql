a92bb39308b5d9c690f675522f40cc319510dc7a
Asma Abu Saa <asmaabusaa4@gmail.com>
2023-02-02 17:11:08 +0200

    fnCompanions Edited

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,11 +1,19 @@
-CREATE FUNCTION fnCompanions (
-	@episodeId int
+CREATE FUNCTION fnCompanions1(
+@episodeId INT
 )
-RETURNS TABLE AS
-Return
-	select CONCAT ('Companion ',ROW_NUMBER() OVER(ORDER BY C.CompanionId ASC),' ',C.CompanionName) AS Companions_Names
-	from tblCompanion as C INNER JOIN tblEpisodeCompanion as EC
-	ON C.CompanionId=EC.CompanionId
-	where EC.EpisodeId=@episodeId;
-   
+RETURNS  varchar(255) AS
+BEGIN
+DECLARE @count INT
+DECLARE @result varchar(255)
+SET @count=1
+SET @result =''
 
+SELECT @result = @result+CONCAT('Companion ',@count)+' '+C.CompanionName+', ',@count=@count+1
+from tblCompanion as C INNER JOIN tblEpisodeCompanion as EC
+	ON C.CompanionId=EC.CompanionId
+	where EC.EpisodeId=@episodeId
+return @result
+END;
+Go
+--SELECT dbo.fnCompanions1(303) as comp;
+--Go
